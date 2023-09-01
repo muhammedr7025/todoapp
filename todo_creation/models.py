d532a107dce611a524290661fa425a8961a8a197
muhammedr7025 <muhammedr7025@gmail.com>
2023-09-02 02:51:26 +0530

    ui changed in the pages signup,login and profile

--- todo_creation/models.py
+++ todo_creation/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Todo(models.Model):
     title = models.CharField(max_length=100)
     desc = models.CharField(max_length=250)
     date = models.DateField()
+    
